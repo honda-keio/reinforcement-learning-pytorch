36ade44bd533f6ba4fcaaf28f42e5e1f4c4e7d78
honda-keio <honda@ykw.keio.ac.jp>
2019-03-26 13:28:34 +0900

    sep

--- dqn/BaseAlgo.py
+++ dqn/BaseAlgo.py
@@ -1,46 +1,68 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch import optim
-import gym
-import random, time
+import random
 import numpy as np
 from baselines.common.vec_env.subproc_vec_env import SubprocVecEnv
 
-class BaseAlgo:
-    def __init__(self, ENV, model, max_epochs, N, T, make_env, t_step=1, optimizer=optim.Adam, n_mid=512, 
-                gamma=0.99, lambda_gae=0.98, v_coef=0.5, ent_coef=0.01, max_grad_norm=0.5, lr=7e-4, device=None):
-        self.envs = SubprocVecEnv([make_env(ENV) for _ in range(N)])
-        self.ob_s = self.envs.observation_space.shape
-        self.t_step = t_step
+class Base:
+    def __init__(self, ENV, model, N, T, make_env, VecEnv=SubprocVecEnv, storage_size=int(1e4), optimizer=optim.Adam, n_mid=512, batch_size=32,
+                gamma=0.99, max_grad_norm=0.5, lr=7e-4, device=None, *args, **kwargs):
+        self.envs = VecEnv([make_env(ENV) for _ in range(N)])
         ac_s = self.envs.action_space.n
+        self.ob_s = self.envs.observation_space.shape
         self.model = model(self.ob_s, ac_s, n_mid=n_mid)
+        self.storage_size = storage_size
         self.optimizer = optimizer(self.model.parameters(), lr=lr)
-        self.max_epochs = max_epochs
         self.N = N
         self.T = T
+        self.batch_size = batch_size
         self.gamma = gamma
-        self.lambda_gae = lambda_gae
-        self.v_coef = v_coef
-        self.ent_coef = ent_coef
         self.max_grad_norm = max_grad_norm
         if device:
             self.device = device
             self.model.to(self.device)
         else:
             self.device = torch.device("cpu")
-        self._init_strage()
 
     def reset_seed(self, seed=0):
         torch.manual_seed(seed)
         random.seed(seed)
         np.random.seed(seed)
 
+class BaseAlgo(Base):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.storage = Storage(ob_s=self.ob_s, *args, **kwargs)
+
+    def one_step(self, t):
+        with torch.no_grad():
+            action = self.model.act(x=self.storage.states[t].to(self.device)).to("cpu").view(-1).numpy()
+            state, reward, done, _ = self.envs.step(action)
+            self.storage.insert(state, done, reward, action, t)
+
+    def calc_loss(self, *args, **kwargs):
+        pass
+    
+    def update(self, *args, **kwargs):
+        self.optimizer.zero_grad()
+        self.calc_loss(*args, **kwargs).backward()
+        nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+        self.optimizer.step()
+        
+
+class Storage:
+    def __init__(self, storage_size, N, ob_s, *args, **kwargs):
+        self.storage_size = storage_size
+        self.N = N
+        self.ob_s = ob_s
+        self._init_strage()
+        
     def _init_strage(self):
-        self.states = torch.zeros([self.T+1, self.N, *self.ob_s])
-        self.masks = np.ones([self.T, self.N, 1], dtype=np.float32)
-        self.rewards = np.zeros([self.T, self.N], dtype=np.float32)
-        self.actions = np.zeros([self.T, self.N], dtype=np.int32)
+        self.states = torch.zeros([self.storage_size+1, self.N, *self.ob_s])
+        self.masks = np.ones([self.storage_size, self.N, 1], dtype=np.float32)
+        self.rewards = np.zeros([self.storage_size, self.N], dtype=np.float32)
+        self.actions = np.zeros([self.storage_size, self.N], dtype=np.int32)
     
     def insert(self, state, done, reward, action, t):
         if not hasattr(state, "numpy"):
@@ -49,9 +71,3 @@ class BaseAlgo:
         self.masks[t][done] = 0.0
         self.rewards[t] = reward
         self.actions[t] = action
-
-    def one_step(self, t):
-        with torch.no_grad():
-            action = self.model.act(x=self.states[t].to(self.device)).to("cpu").view(-1).numpy()
-            state, reward, done, _ = self.envs.step(action)
-            self.insert(state, done, reward, action, t)
